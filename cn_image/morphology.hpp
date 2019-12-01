b4dd44dda62cc66f299cdf07fc1619935f8f203c
Clara Nguyen <idesty@derpg.xyz>
2019-12-01 00:49:51 -0500

    Added a very simplistic remake of "morphology_.java" in C++

---
+++ cn_image/morphology.hpp
@@ -0,0 +1,104 @@
+/*
+ * Morphology
+ *
+ * Description:
+ *     Implements the Morphology plugin given in "morphology_.java". This is
+ *     simply a rewrite of it in C++.
+ *
+ * Author:
+ *     Clara Nguyen
+ */
+
+#ifndef __CN_IMG_MORPHOLOGY_HPP__
+#define __CN_IMG_MORPHOLOGY_HPP__
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+#include "image_processor.hpp"
+#include "basics.hpp"
+
+using namespace std;
+
+namespace cn_image {
+
+	/*
+	 * dilate
+	 *
+	 * Performs Dilation on a given image. This assumes the image has been run
+	 * through "binarise" already.
+	 */
+
+	template <typename T>
+	void dilate(image_processor<T> &img, int rad) {
+		image_processor<T> tmp;
+		tmp.resize(img.get_width(), img.get_height());
+
+		vector< vector<T> > &pixels = img.ext_get_pixels();
+		vector< vector<T> > &tmp_pixels = tmp.ext_get_pixels();
+
+		int n, m, i, j, r;
+
+		//Repeat "rad" times
+		for (r = 0; r < rad; r++) {
+			//For every pixel in the image
+			for (j = 0; j < img.get_height(); j++) {
+				for (i = 0; i < img.get_width(); i++) {
+					/*
+					 * Loop through surrounding pixels to see if any match. If
+					 * so, set. Otherwise, don't.
+					 */
+
+					for (n = max(j - 1, 0); n < min(j + 1, (int) img.get_height()); n++) {
+						for (m = max(i - 1, 0); m < min(i + 1, (int) img.get_width()); m++) {
+							if (pixels[n][m] == 255)
+								tmp_pixels[j][i] = 255;
+						}
+					}
+				}
+			}
+
+			pixels = tmp_pixels;
+		}
+	}
+
+	/*
+	 * erode
+	 *
+	 * Performs Erosion on a given image.
+	 */
+
+	template <typename T>
+	void erode(image_processor<T> &img, int rad) {
+		invert(img);
+		dilate(img, rad);
+		invert(img);
+	}
+
+	/*
+	 * open
+	 *
+	 * Performs an erosion followed by a dilation
+	 */
+
+	template <typename T>
+	void open(image_processor<T> &img, int rad) {
+		erode(img, rad);
+		dilate(img, rad);
+	}
+
+	/*
+	 * close
+	 *
+	 * Performs a dilation followed by an erosion
+	 */
+
+	template <typename T>
+	void close(image_processor<T> &img, int rad) {
+		dilate(img, rad);
+		erode(img, rad);
+	}
+}
+
+#endif
